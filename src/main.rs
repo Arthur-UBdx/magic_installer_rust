7d82c0bd49a425383c38e1ead08162b4aba36395
Arthur-UBdx <arthur.richelet@gmail.com>
2023-01-25 23:57:05 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+// extern crate native_windows_gui as nwg;
+// extern crate native_windows_derive as nwd;
+
+
+use native_windows_gui::NativeUi;
+use native_windows_gui as nwg;
+use native_windows_derive as nwd;
+
+use magic_installer::*;
+use std::sync::mpsc;
+use std::thread;
+// use std::thread::sleep;
+// use std::time::Duration;
+
+#[tokio::main]
+async fn main() -> ! {
+    // let config: Config = Config::from((String::new(),String::from("https://github.com/Arthur-UBdx/fabric_minecraft/zipball/main"),String::new()));
+    let config: Config = Config::new();
+    download_mods(config);
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+}
+
